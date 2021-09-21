2b6d26e65fcc2ebf5391003ed7c1bdc3fdab4229
Alexander-Yershov <alex04398956@gmail.com>
2021-09-21 21:11:02 +0200

    Update script-21.js

--- js/script-21.js
+++ js/script-21.js
@@ -4,10 +4,12 @@ function findLongestWord(string) {
   let longestWord = strSplit[0];
   
   for (let i = 0; i < strSplit.length; i += 1){
-    if (strSplit[i].length > longestWord.length)
+    if (strSplit[i].length > longestWord.length){
       longestWord = strSplit[i];
+    }
   }
 return longestWord;
 
   // Change code above this line
 }
+console.log(findLongestWord("DevTools failed to load source map"))
